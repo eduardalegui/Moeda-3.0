a3c581819901d1aedd85d26b21f9543f4e90fa81
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-03-20 18:49:24 -0300

    02

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiKey = "37a26a6057489965c5ad1d60";
 
 
-window.onload = async function () {
+window.onload = async function (){
     let data = await fetch("https://v6.exchangerate-api.com/v6/37a26a6057489965c5ad1d60/latest/USD");
     if(!data.ok){
         return
