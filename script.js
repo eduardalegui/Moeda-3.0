feed0622a695073b0396dacf5d03b1a7e8783600
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-03-20 15:30:13 -0300

    moeda 1

---
+++ script.js
@@ -0,0 +1,99 @@
+const apiKey = "37a26a6057489965c5ad1d60";
+
+
+window.onload = async function () {
+    let data = await fetch("https://v6.exchangerate-api.com/v6/37a26a6057489965c5ad1d60/latest/USD");
+    if(!data.ok){
+        return
+    }
+    let jsonData = await data.json();
+    let keys = Object.keys(jsonData.conversion_rates);
+
+    var select = document.getElementById("selectFrom");
+    
+    var select2 = document.getElementById("selectFrom2");
+    
+    keys.forEach(key => {
+        var option = document.createElement('option');
+        option.text = option.value = key;
+        var option2 = document.createElement('option');
+        option2.text = option2.value = key;
+        
+        select.add(option, key);
+        select2.add(option2, key);
+        
+        console.log(key);  
+    });
+    
+        
+    
+}
+
+async function convert(){
+    
+    let moeda1 = document.getElementById("selectFrom").value;
+    let moeda2 = document.getElementById("selectFrom2").value;
+    
+    let input1 = document.getElementById("input").value;
+    
+    let data = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/pair/${moeda1}/${moeda2}`);
+
+    console.log("data")
+    let jsonData = await data.json();
+
+    console.log(jsonData);
+    document.getElementById("input2").value = (jsonData.conversion_rate * input1).toFixed(2);
+
+
+}
+
+
+
+
+async function getRates() {
+
+    let data = await fetch("https://v6.exchangerate-api.com/v6/37a26a6057489965c5ad1d60/latest/USD");
+        if(!data.ok){
+            return
+        }
+    let jsonData = await data.json();
+    console.log(jsonData);
+
+    let keys = Object.keys(jsonData.conversion_rates);
+
+    console.log("-----------------");
+    console.log("conversion_rates.keys:", keys);
+    console.log("Taxa para EUR:", jsonData.conversion_rates.EUR);
+    console.log("Taxa para BRL:", jsonData.conversion_rates.BRL);
+
+
+    /*
+    var select = document.getElementById("selectFrom");
+    for (key in keys){
+        var option = document.createElement('option');
+        option.text = option.value = key;
+        select.add(option, keys[key]);
+        console.log(keys[key]);     
+    }
+    */
+
+    // let todos = document.getElementById('selectFrom');
+    // let newDefault1 = new Option('Select todo', null, true, true)
+    // newDefault1.disabled = true
+    // todos.add(newDefault1)
+
+    
+    //     .then(res => res.json())
+    //     .then(data => {
+    //     data.forEach(todo => {
+    //         let option = new Option(todo.title, todo.id)
+    //         console.log(option)
+    //         todos.add(option)
+    //     });
+    // });
+}
+        
+        
+        
+    //document.getElementById('selectFrom') = jsonData.conversion_rates.BRL;
+
